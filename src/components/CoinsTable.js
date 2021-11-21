34a4d41066c2ce88c6723e1711bec17ac9d22110
Vijayraj-wd <vijayrajck@outlook.com>
2021-11-21 17:25:39 +0530

    COMPONENTS: COINS TABLE

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -1,7 +1,186 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { CoinList } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+import {
+  Container,
+  createTheme,
+  LinearProgress,
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TextField,
+  ThemeProvider,
+  Typography,
+  TableBody,
+  makeStyles,
+} from "@material-ui/core";
+
+export function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 const Coinstable = () => {
-  return <div>Coinstable</div>;
+  const [Coins, setCoins] = useState([]);
+  const [Loading, setLoading] = useState(false);
+  const [Search, setSearch] = useState([]);
+  const { Currency, Symbol } = CryptoState();
+  const history = useHistory();
+
+  const fetchCoins = async () => {
+    setLoading(true);
+    const { data } = await axios.get(CoinList(Currency));
+    setCoins(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchCoins();
+  }, [Currency]);
+
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      type: "dark",
+    },
+  });
+
+  const handlesearch = () => {
+    return Coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(Search) ||
+        coin.Symbol.toLowerCase().includes(Search)
+    );
+  };
+
+  const useStyles = makeStyles(() => ({
+    row: {
+      backgroundColor: "#16171a",
+      cursor: "pointer",
+      "&:hover": {
+        backgroundColor: "#131111",
+      },
+      fontFamily: "Montserrat",
+    },
+  }));
+
+  const classes = useStyles();
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container style={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          style={{ margin: 18, fontFamily: "Montserrat" }}
+        >
+          Cryptocurrency Prices by Market Cap
+        </Typography>
+        <TextField
+          label="Search for a crypto currency..."
+          variant="outlined"
+          style={{ marginBottom: 20, width: "100%" }}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <TableContainer>
+          {Loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <>
+              <Table>
+                <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                  <TableRow>
+                    {["Coin", "Price", "24h Change", "Market Cap"].map(
+                      (head) => (
+                        <TableCell
+                          style={{
+                            color: "black",
+                            fontWeight: "700",
+                            fontFamily: "Montserrat",
+                          }}
+                          key={head}
+                          align={head === "Coin" ? "" : "right"}
+                        >
+                          {head}
+                        </TableCell>
+                      )
+                    )}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {handlesearch().map((row) => {
+                    const profit = row.price_change_percentage_24h > 0;
+
+                    return (
+                      <TableRow
+                        onClick={() => history.push(`/coins/${row.id}`)}
+                        className={classes.row}
+                        key={row.name}
+                      >
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          styles={{
+                            display: "flex",
+                            gap: 15,
+                          }}
+                        >
+                          <img
+                            src={row?.image}
+                            alt={row.name}
+                            height="50"
+                            style={{ marginBottom: 10 }}
+                          />
+                          <div
+                            style={{ display: "flex", flexDirection: "column" }}
+                          >
+                            <span
+                              style={{
+                                textTransform: "uppercase",
+                                fontSize: 22,
+                              }}
+                            >
+                              {row.symbol}
+                            </span>
+                            <span style={{ color: "darkgrey" }}>
+                              {row.name}
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell align="right">
+                          {Symbol}{" "}
+                          {numberWithCommas(row.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                            fontWeight: 500,
+                          }}
+                        >
+                          {profit && "+"}
+                          {row.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell align="right">
+                          {Symbol}{" "}
+                          {numberWithCommas(
+                            row.market_cap.toString().slice(0, -6)
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </>
+          )}
+        </TableContainer>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default Coinstable;
